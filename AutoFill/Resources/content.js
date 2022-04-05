10abe9fce0fc8d5831a42b3b9931c4ca10866559
Saravanakumar <smurugesan@ntst.com>
2022-04-05 17:19:36 +0530

    Added primary, secondary and anonmyous hardcoded data

--- AutoFill/Resources/content.js
+++ AutoFill/Resources/content.js
@@ -3,10 +3,13 @@ const primary = "primary"
 const setPrimaryValue = () =>
 {
     fill("email", "hiring@iomd.info")
-    fill("password", "pandi@123")
+    fill("password", "test@123")
     fill("firstName", "Rohan")
+    fill("first_name", "Rohan")
     fill("lastName", "Mahadevan")
+    fill("last_name", "Mahadevan")
     fill("phone", "4085551212")
+    fill("location", "900 Menlo Oaks Drive")
     fill("address1", "900 Menlo Oaks Drive")
     fill("street", "900 Menlo Oaks Drive")
     fill("city", "Menlo Park")
@@ -14,20 +17,98 @@ const setPrimaryValue = () =>
     fill("state", "CA")
     fill("postalCode", "94025")
     fill("postCode", "94025")
+    fill("postal-Code", "94025")
+    
+    fill("cc-number", "5459612890007890")
+    fill("credit-card-number", "5459612890007890")
+    fill("creditcardnumber", "5459612890007890")
+    fill("cardnumber", "5459612890007890")
+    fill("encryptedCardNumber", "5459612890007890")
+   
+    fill("cs-csc","123")
+    fill("cvv","123")
+    fill("encryptedSecurityCode","123")
+    
+    fill("cc-exp","04/24")
+    fill("expirationDate","04/24")
+    fill("expiration","04/24")
+    fill("encrptedExpiryDate","04/24")
+   
+}
+
+const setSecondaryValue = () =>
+{
+    fill("email", "test3.ideas2it@iomd.info")
+    fill("password", "test@123")
+    fill("firstName", "Harsha")
+    fill("first_name", "Harsha")
+    fill("lastName", "Ideas2it")
+    fill("last_name", "Ideas2it")
+    fill("phone", "4085551212")
+    fill("location", "361 Linfield drive")
+    fill("address1", "361 Linfield drive")
+    fill("street", "361 Linfield drive")
+    fill("city", "Menlo Park")
+    fill("country", "US")
+    fill("state", "CA")
+    fill("postalCode", "94025")
+    fill("postCode", "94025")
+    fill("postal-Code", "94025")
+   
+    fill("cc-number", "5459612890007174")
+    fill("credit-card-number", "5459612890007174")
+    fill("creditcardnumber", "5459612890007174")
+    fill("cardnumber", "5459612890007174")
+    fill("encryptedCardNumber", "5459612890007174")
+    
+    fill("cs-csc","456")
+    fill("cvv","456")
+    fill("encryptedSecurityCode","456")
+
+    
+    
+    fill("cc-exp","07/22")
+    fill("expirationDate","07/22")
+    fill("encrptedExpiryDate","07/22")
+    fill("expiration","07/22")
+   
+    
 }
 
 const setAnonymousValue = () =>
 {
-    fill("email", "test1.ideas2it@iomd.info")
-    fill("password", "sample@123")
-    fill("firstName", "Rohan")
-    fill("lastName", "Mahadevan")
+    fill("email", "test2.ideas2it@iomd.info")
+    fill("password", "test@123")
+    fill("firstName", "Test2")
+    fill("first_name", "Test2")
+    fill("lastName", "Ideas2it")
+    fill("last_name", "Ideas2it")
     fill("phone", "4085551212")
-    fill("address1", "361 Linfield drive")
-    fill("city", "menlo park")
-    fill("state", "ca")
-    fill("country", "United States")
+    fill("location", "Belle Haven Branch Library")
+    fill("address1", "Belle Haven Branch Library")
+    fill("street", "Belle Haven Branch Library")
+    fill("city", "Menlo Park")
+    fill("country", "US")
+    fill("state", "CA")
     fill("postalCode", "94025")
+    fill("postCode", "94025")
+    fill("postal-Code", "94025")
+   
+    fill("cc-number", "4207399013131009")
+    fill("credit-card-number", "4207399013131009")
+    fill("creditcardnumber", "4207399013131009")
+    fill("cardnumber", "4207399013131009")
+    fill("encryptedCardNumber", "4207399013131009")
+   
+    fill("cs-csc","345")
+    fill("cvv","345")
+    fill("encryptedSecurityCode","345")
+    
+    fill("cc-exp","04/23")
+    fill("expirationDate","04/23")
+    fill("expiration","04/23")
+    fill("encrptedExpiryDate","04/23")
+    
 }
 
 function fill(name, value) {
@@ -63,12 +144,29 @@ function autofill(element, value) {
     return element
 }
 
-browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
+//browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
+//    alert("requsting", request)
+////        setPrimaryValue()
+////    if (request.key == primary){
 //        setPrimaryValue()
-//    if (request.key == primary){
+////    }else{
+////        setAnonymousValue()
+////    }
+//});
+
+browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
+//    alert("Test")
+//    alert(JSON.stringify(request))
+//    alert(request)
+    if (request == "primary"){
         setPrimaryValue()
-//    }else{
-//        setAnonymousValue()
-//    }
+    }else if (request == "secondary"){
+        setSecondaryValue()
+    }else if (request == "anonmyous"){
+        setAnonymousValue()
+    }else{
+        setPrimaryValue()
+    }
+    
 });
 
--- autofillboard/ViewController.swift
+++ autofillboard/ViewController.swift
@@ -9,8 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var versionLabel : UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.title = "Home"
+        if let version = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String {
+            if let appVersion = Bundle.main.infoDictionary!["CFBundleVersion"] as? String{
+                self.versionLabel.text = "Version \(version).\(appVersion)"
+            }else{
+                self.versionLabel.text = "Version \(version)"
+            }
+           
+       }
         // Do any additional setup after loading the view.
     }
 
@@ -25,6 +35,14 @@ class ViewController: UIViewController {
                         print("Settings opened: \(success)") // Prints true
                     })
     }
+    }
+    
+    @IBAction func onSafari(){
+        guard let tedBakerURL = URL(string: "https://www.tedbaker.com") else {
+                    return
+                }
+        
+        UIApplication.shared.openURL(tedBakerURL as URL)
     }
 
 }
